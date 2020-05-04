099951038f74d23ec973465c6af0859a312c1981
박종한 <hani12p@naver.com>
2020-05-05 00:39:44 +0900

    Chapter 7 장

---
+++ opencv4/Project1/Project1.cpp
@@ -0,0 +1,74 @@
+﻿// Project1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+/*
+int& CalSum(int s, int e);
+int main()
+{
+
+   //int i = 0;
+   //int& r = i;
+
+   //r = 9; //i=9와 같은 결과(r변수는 i변수를 참조)
+   //cout << "i:" << i << endl;
+   //return 0;
+
+
+   int& r1 = CalSum(1,100);
+   int &r2 = CalSum(1,10);
+   cout << "r1:" << r1 << ",r2:" <<r2<< endl;
+
+   return 0;
+}
+
+int& CalSum(int s, int e)
+{
+   int sum = 0;
+   for (; s <= e; s++)
+   {
+      sum += s;
+   }
+   return sum;
+}
+*/
+class  test
+{
+public :
+    int a;
+
+};
+
+
+//기본자료형  char int 
+
+
+int Add(int& r1, int& r2);
+int Add(int i, int j);
+int main()
+{
+    int re = Add(2, 3);
+    int a = 2;
+    int b = 4;
+    int& c = a;
+    int& d = b;
+    cout << "re:" << re << endl;
+    
+    re = Add((int&)c,(int&)d);
+
+    cout << "re:" << re << endl;
+    return 0;
+}
+
+
+int Add(int & r1, int & r2)
+{
+    return r1 + r2;
+}
+
+int Add(int i, int j)
+{
+    return i + j;
+}
--- opencv4/chap2_2/main.cpp
+++ opencv4/chap2_2/main.cpp
@@ -1,16 +1,22 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <stdio.h>
+
 
 using namespace cv;
 using namespace std;
 
+
 int main(void) {
 
+	//printf("%s %d ")
 
+	
 	cout << "Hello OpenCV" << CV_VERSION << endl;
 
 	Mat img;
-	img = imread("lenna.bmp");
+
+	img = imread("lenna.bmp"); 
 
 	if (img.empty())
 	{
@@ -20,6 +26,7 @@ int main(void) {
 
 	namedWindow("image");
 	imshow("image", img);
+	imshow("image2", img);
 	
 	waitKey();
 	return 0;
